b66ecc75a49e6fb4b5de762b0776c2fe0a5379b6
Soufiane AKNOUCHE <soufianeaknouche90@gmail.com>
2023-09-11 10:03:09 +0200

    Database and product - category entities creation

---
+++ src/main/java/com/sadev/app/model/Product.java
@@ -0,0 +1,86 @@
+package com.sadev.app.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, unique = true)
+	private String name;
+	@Column(nullable = false)
+	private String description;
+	@Column(nullable = false)
+	private String price;
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	public Product() {
+		super();
+	}
+
+	public Product(String name, String description, String price) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+				+ ", category=" + category + "]";
+	}
+
+}
