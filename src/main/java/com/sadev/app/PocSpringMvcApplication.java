0b56a7f4069c0aa9f9242537d0be17b98f533da4
Soufiane AKNOUCHE <soufianeaknouche90@gmail.com>
2023-09-11 09:39:04 +0200

    first commit

---
+++ src/main/java/com/sadev/app/PocSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.sadev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringMvcApplication.class, args);
+	}
+
+}
